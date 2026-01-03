dae8f47a1f851dbfab401fb382e3a38a03e69193
DMDanser <155984953+DMDanser@users.noreply.github.com>
2026-01-02 20:44:48 -0500

    Reformat
    -about added

---
+++ PortfolioCode/script.js
@@ -0,0 +1,9 @@
+function Test1()
+{
+    const ToggleText = document.getElementById("ToggleText");
+    ToggleText.innerHTML = "On";
+}
+function PlayVideo()
+{
+    const game = document.querySelector(".gameImage");
+}
