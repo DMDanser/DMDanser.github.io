bf1d4949f884bb59c134a73246a975e7a1c198cc
DMDanser <155984953+DMDanser@users.noreply.github.com>
2026-01-02 21:07:07 -0500

    Remove yml

--- PortfolioCode/script.js
+++ PortfolioCode/script.js
@@ -3,7 +3,3 @@ function Test1()
     const ToggleText = document.getElementById("ToggleText");
     ToggleText.innerHTML = "On";
 }
-function PlayVideo()
-{
-    const game = document.querySelector(".gameImage");
-}
